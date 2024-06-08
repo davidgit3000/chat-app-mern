67506831757fb58bf0399373d4e7697a572f7d37
davidgit3000 <davidkarik5@gmail.com>
2024-06-07 20:56:33 -0700

    fixed responsive height of chat panel in desktop view

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex sm:flex-col sm:overflow-auto lg:flex-row md:h-[30rem] md:w-[40rem] lg:h-[35rem] lg:w-[70rem] xl:w-[70rem] xl:h-[35rem] rounded-lg 2xl:h-[100rem] 2xl:w-[100rem] overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className="flex sm:flex-col sm:overflow-auto lg:flex-row md:h-[30rem] md:w-[40rem] lg:h-[35rem] lg:w-[70rem] xl:w-[70rem] xl:h-[35rem] rounded-lg 2xl:h-[70rem] 2xl:w-[100rem] overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
       <MessageContainer />
     </div>
