e2af0688b3328cdae5a74f3c7fa8663263d2f832
davidgit3000 <davidkarik5@gmail.com>
2024-06-08 21:00:47 -0700

    added date on each message

--- frontend/src/utils/extractTime.js
+++ frontend/src/utils/extractTime.js
@@ -1,8 +1,11 @@
 export function extractTime(dateString) {
   const date = new Date(dateString);
+  const day = padZero(date.getDate());
+  const month = padZero(date.getMonth());
+  const year = padZero(date.getFullYear());
   const hours = padZero(date.getHours());
   const minutes = padZero(date.getMinutes());
-  return `${hours}:${minutes}`;
+  return `${month}/${day}/${year}, ${hours}:${minutes}`;
 }
 
 // Helper function to pad single-digit numbers with a leading zero
